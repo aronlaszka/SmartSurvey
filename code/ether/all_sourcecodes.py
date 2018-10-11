f0d7f509fbf9e614eac512e704154a3104a298d3
sarahalex98 <40644506+sarahalex98@users.noreply.github.com>
2018-10-11 11:10:10 -0500

    Update all_sourcecodes.py

--- code/ether/all_sourcecodes.py
+++ code/ether/all_sourcecodes.py
@@ -1,24 +1,30 @@
 import csv
 import urllib.request
-from bs4 import BeautifulSoup
 
-with open('etherscanlist2.csv') as csvfile:
+# waiting for list of contracts to be updated so that this code can be checked
+with open('etherscanlist3.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        Address = row['Address_']
+        Address = row['Address']
         # print(Address)
         req = urllib.request.Request(url='https://etherscan.io/address/' + str(Address),
                              data=None,
                              headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4'
                                       })
-        with urllib.request.urlopen(req) as response, open('file.txt', 'a') as out_file:
-            # making out_file a list so that it can be appended and written into
-            out_file = []
-            data = response.read()  # a `bytes` object
-            # print(data)
-            # out_file.write(data)
-            soup = BeautifulSoup(data, "lxml")
-            for info in soup.findAll('pre', {'class': 'js-sourcecopyarea', 'id': 'editor'}):
-                b = info.text.encode('utf-8')
-                print(b)
-                out_file.append(b)
+        # Specify user agent to gain access
+        sleep(randint(8, 15))
+        # sleep used to slow down the url request
+        response = urllib.request.urlopen(req)
+        responseData = response.read()
+        # removed decode statement so that data is in bytes
+        print(responseData)
+
+        with open(str(Address) + ".htm", 'wb') as f_out:
+            f_out.write(bytes(responseData))
+
+            # soup = BeautifulSoup(data, "lxml")
+            # for info in soup.findAll('pre', {'class': 'js-sourcecopyarea', 'id': 'editor'}):
+                # b = info.text.encode('utf-8')
+                # print(b)
+                # out_file.append(b)
+
