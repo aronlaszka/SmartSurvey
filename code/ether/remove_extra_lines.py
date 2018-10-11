27d248e89250078c5f789f11800bb08ae603be33
sarahalex98 <40644506+sarahalex98@users.noreply.github.com>
2018-10-11 11:08:05 -0500

    Update remove_extra_lines.py

--- code/ether/remove_extra_lines.py
+++ code/ether/remove_extra_lines.py
@@ -1,16 +1,25 @@
-    with open('etherscan1.csv', 'r') as file_handle:
-        lines = file_handle.readlines()
+# SECTION 2-------------------------------------------------------------------------------------
+# Remove blank lines
+import csv
 
-    with open('etherscanlist1.csv', 'w') as file_handler:
-        lines = filter(lambda x: x.strip(), lines)
-        file_handler.writelines(lines)
+with open('etherscan1new.csv', 'r') as file_handle:
+    lines = file_handle.readlines()
+with open('etherscanlist1new.csv', 'w') as file_handler:
+    lines = filter(lambda x: x.strip(), lines)
+    file_handler.writelines(lines)
 
-    rows = ['Address', 'Contract Name', 'Compiler', 'Balance', 'Tx count', 'Date verified']
-    with open('etherscanlist1.csv', 'r') as old_file, open('etherscanlist2.csv', 'w') as new_file:
-        writer = csv.DictWriter(new_file, delimiter=' ', lineterminator='\n',
-                                fieldnames=['Address_', 'Contract_Name', 'Compiler_', 'Balance_', 'Tx_count',
-                                            'Date_verified'])
-        writer.writeheader()
-        for line in old_file:
-            if not any(row in line for row in rows):
-                new_file.write(line)
+# Remove extra header rows starting with Address
+rows = ['Address', 'Contract Name', 'Compiler', 'Balance', 'Tx count', 'Date verified']
+with open('etherscanlist1new.csv', 'r') as inp, open('etherscanlist2new.csv', 'w', newline='') as out:
+    writer = csv.writer(out)
+    for row in csv.reader(inp):
+        if row[0] != "Address":
+            writer.writerow(row)
+
+# Add one final header row to the final list
+with open('etherscanlist2new.csv', 'r') as inp1, open('etherscanlist3new.csv', 'w', newline='') as out1:
+    fieldnames = ['Address', 'Contract Name', 'Compiler', 'Balance', 'Tx count', 'Date verified']
+    writer = csv.DictWriter(out1, fieldnames=fieldnames)
+    writer.writeheader()
+    lines = inp1.readlines()
+    out1.writelines(lines)
