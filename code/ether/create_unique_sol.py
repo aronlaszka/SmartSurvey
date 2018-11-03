eee6f5b5fb475bd68203c0720621bcb1b8a617ad
sarahalex98 <40644506+sarahalex98@users.noreply.github.com>
2018-11-02 23:48:19 -0500

    Update create_unique_sol.py

--- code/ether/create_unique_sol.py
+++ code/ether/create_unique_sol.py
@@ -7,6 +7,10 @@ import re
 import pyparsing
 
 d = {}
+# Initializing dictionary d containing {sourcecode string f: x + .sol}
+uniques = {}
+# Initializing dictionary uniques to store addresses x with unique source codes
+
 
 with open(r'etherscanlist4new.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
@@ -15,11 +19,15 @@ with open(r'etherscanlist4new.csv') as csvDataFile:
         data = [row[0] for row in csv.reader(csvDataFile)]
         # data includes the numbers till n-1 for example [0:2] that displays 1st and second column
         # of 0th/1st row in csv file
-        Address = data[0:6] # 46188]
+        Address = data[0:46188] # 46188]
+        # print(type(Address))
+        # print(len(Address))
         for x in Address:
+            # print(type(x))
             # print(x) here x is all the 42 bit addresses
             # creating the solidity file
-            with open(x + ".htm", 'r', encoding='utf-8') as f_input: # open(x + ".sol", 'w', encoding='utf-8') as f_output:
+            with open(x + ".htm", 'r', encoding='utf-8') as f_input:
+                # open(x + ".sol", 'w', encoding='utf-8') as f_output:
                 # print(f_input)
                 soup = BeautifulSoup(f_input, "html.parser")
                 for info in soup.findAll('pre', {'class': 'js-sourcecopyarea', 'id': 'editor'}):
@@ -27,59 +35,45 @@ with open(r'etherscanlist4new.csv') as csvDataFile:
                     # # print(b)
                     # clean white space and tabs
                     b = b.replace(' ', '')
-                    comment = pyparsing.nestedExpr("/*", "*/").suppress()
-                    c = comment.transformString(b)
+                    # comment = pyparsing.nestedExpr("/*", "*/").suppress()
+                    # c = comment.transformString(b)
                     # print(c)
-                    c = c.split()
+                    c = b.split()
                     e = [x for x in c if not '//' in x]
                     f = ''.join(e)
                     f = f.replace(' ', '').replace('\n', '').strip().rstrip().lstrip()
                     # f is the string of the contract without whitespaces and comments
                     # print(f)
-
-                    # d[f] = 1
-
                     # print(d)
                     if f in d:
                         # print(d)
-                        print(f"{x} not unique!  first: {d[f]}")
+                        print(f"{x} not unique! first: {d[f]}")
                     else:
                         d[f] = x + ".sol"
+                        # print(d[f])
                         # creating the unique solidity files with addressname.sol
-                        # with open('etherscanlist4new.csv', 'r') as f_read, open(x + ".sol", 'w') as f_write:
-                        #     f_write.write(f)
+                        with open('etherscanlist4new.csv', 'r') as f_read, open(x + ".sol", 'w') as f_write:
+                            f_write.write(f)
+                        # uniques.append(d[f])
+                    uniques[x] = d[f] # assigning values of address dictionary to string dictionary
 
-                        with open('etherscanlist4new.csv', 'r') as f_read1, open('etherscanlist5new.csv', 'w') as f_write1:
-                            rows = []
-                            for row_all in f_read1:
-                                if row_all in rows:
-                                    continue
-                                else:
-                                    f_write1.write(row_all)
-                                    rows.append(row_all)
-                        f_read1.close()
-                        f_write1.close()
+with open('etherscanlist4new.csv', 'r') as csvinput, open('etherscanlist5new.csv',  'w') as csvoutput:
+    writer = csv.writer(csvoutput, lineterminator='\n')
+    reader = csv.reader(csvinput)
 
-                        with open('etherscanlist5new.csv', 'w') as f_write2:
-                            row[6] = d[f]
-                            rows.append(row[6])
+    row = next(reader) # selecting row next to last row
+    row.append('Unique solidity files') # header
+    # all.append(row)
+    writer.writerow(row)
 
-                        # with open('etherscanlist5new.csv', 'r') as fin_read, open('etherscanlist5new.csv', 'w') as fout_write:
-                        #     reader = csv.reader(fin_read)
-                        #     writer = csv.writer(fout_write)
-                        #     for row2 in reader:
-                        #         row[6] = d[f]
-                        #         writer.writerow(row2)
-                        #     fin_read.close()
-                        #     fout_write.close()
+    print(row)
 
-                        # if f in d:
-                        #     # print(f)
-                        #     # print(len(d))
-                        #     print(f"{x} not unique! first: {d[f]}")
-                        #     # print("yes")
-                        # else:
-                        #     # write b in to file
-                        #     d[f] = x + ".sol"
-                        #     print(d[f])
-                        #     # f_output.write(f)
+    for row in reader:
+        row.append(uniques[row[0]] if row[0] in uniques else '')
+        # appending unique files from addresses contracts in row 0
+        # all.append(row)
+        writer.writerow(row)
+
+    writer.writerow(row)
+
+
