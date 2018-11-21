3c4ced6abc1f03953b309d8d0e5e39aaa25816f8
sarahalex98 <40644506+sarahalex98@users.noreply.github.com>
2018-11-21 14:00:18 -0600

    Update create_unique_sol.py

--- code/ether/create_unique_sol.py
+++ code/ether/create_unique_sol.py
@@ -4,13 +4,28 @@
 from bs4 import BeautifulSoup
 import csv
 import re
-import pyparsing
+import html
 
 d = {}
 # Initializing dictionary d containing {sourcecode string f: x + .sol}
 uniques = {}
 # Initializing dictionary uniques to store addresses x with unique source codes
 
+def remove_comments(string):
+    pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"
+    # the first group captures quoted strings (double or single)
+    # the second group captures comments (//single-line or /* multi-line */)
+    regex = re.compile(pattern, re.MULTILINE | re.DOTALL)
+
+    def _replacer(match):
+        # if the 2nd group (capturing comments) is not None,
+        # it means we have captured a non-quoted (real) comment string.
+        if match.group(2) is not None:
+            return ""  # returns empty to remove the comment
+        else:  # otherwise, returns the 1st group
+            return match.group(1)  # captured quoted-string
+
+    return regex.sub(_replacer, string)
 
 with open(r'etherscanlist4new.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
@@ -19,43 +34,41 @@ with open(r'etherscanlist4new.csv') as csvDataFile:
         data = [row[0] for row in csv.reader(csvDataFile)]
         # data includes the numbers till n-1 for example [0:2] that displays 1st and second column
         # of 0th/1st row in csv file
-        Address = data[0:46188] # 46188]
+        Address = data[0:46188]
+        # 46188
         # print(type(Address))
         # print(len(Address))
         for x in Address:
             # print(type(x))
-            # print(x) here x is all the 42 bit addresses
+            # print(x)
+            # here x is all the 42 bit addresses
             # creating the solidity file
             with open(x + ".htm", 'r', encoding='utf-8') as f_input:
                 # open(x + ".sol", 'w', encoding='utf-8') as f_output:
+                # here f_input is the.htm file
                 # print(f_input)
                 soup = BeautifulSoup(f_input, "html.parser")
                 for info in soup.findAll('pre', {'class': 'js-sourcecopyarea', 'id': 'editor'}):
                     b = info.text
-                    # # print(b)
-                    # clean white space and tabs
-                    b = b.replace(' ', '')
-                    # comment = pyparsing.nestedExpr("/*", "*/").suppress()
-                    # c = comment.transformString(b)
-                    # print(c)
-                    c = b.split()
-                    e = [x for x in c if not '//' in x]
-                    f = ''.join(e)
-                    f = f.replace(' ', '').replace('\n', '').strip().rstrip().lstrip()
-                    # f is the string of the contract without whitespaces and comments
-                    # print(f)
-                    # print(d)
-                    if f in d:
+                    # print(b)
+                    c = html.unescape(b)
+                    # using html unescape to convert html entities such as "&lt" to "<"
+                    u = remove_comments(c)
+                    # print(u)
+                    h = u.replace(' ', '').replace('\n', '').strip().rstrip().lstrip()
+                    # h is the string of the contract without whitespaces and // comments
+                    # print(h)
+                    if h in d:
                         # print(d)
-                        print(f"{x} not unique! first: {d[f]}")
+                        print(f"{x} not unique! first: {d[h]}")
                     else:
-                        d[f] = x + ".sol"
-                        # print(d[f])
+                        d[h] = x + ".sol"
+                        # print(d[h])
                         # creating the unique solidity files with addressname.sol
                         with open('etherscanlist4new.csv', 'r') as f_read, open(x + ".sol", 'w') as f_write:
-                            f_write.write(f)
-                        # uniques.append(d[f])
-                    uniques[x] = d[f] # assigning values of address dictionary to string dictionary
+                            f_write.write(u)
+                        # uniques.append(d[h])
+                    uniques[x] = d[h] # assigning values of address dictionary to string dictionary
 
 with open('etherscanlist4new.csv', 'r') as csvinput, open('etherscanlist5new.csv',  'w') as csvoutput:
     writer = csv.writer(csvoutput, lineterminator='\n')
